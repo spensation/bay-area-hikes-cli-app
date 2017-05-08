5896f452405cffef9fc5986e54d3847857cd33ae
Spencer Evans <spencevans@gmail.com>
2017-05-08 22:59:45 +0000

    working on CLI

--- lib/bay_area_hikes/cli.rb
+++ lib/bay_area_hikes/cli.rb
@@ -2,6 +2,22 @@ class BayAreaHikes::CLI
 
   def call
     puts 'Great hikes in the Bay.'
+    list_hikes
+    menu
   end
 
+  def list_hikes
+    puts <<-DOC
+      1. Mt. Tam
+      2. Briones
+      3. Skyline
+      DOC
+  end
+
+  def menu
+    puts 'Please enter the number of the hike you would like to know more about or exit to exit:'
+  end
+
+
+  end
 end

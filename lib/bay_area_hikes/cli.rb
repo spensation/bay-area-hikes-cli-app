04e45fb74c09400ed10b7d51074eb306ae4a8940
Spencer Evans <spencevans@gmail.com>
2017-05-09 02:10:37 +0000

    cli stubbed out

--- lib/bay_area_hikes/cli.rb
+++ lib/bay_area_hikes/cli.rb
@@ -1,23 +1,36 @@
 class BayAreaHikes::CLI
 
   def call
-    puts 'Great hikes in the Bay.'
     list_hikes
     menu
+    goodbye
   end
 
   def list_hikes
-    puts <<-DOC
-      1. Mt. Tam
-      2. Briones
-      3. Skyline
-      DOC
+    puts 'Great ideas for a hike in the Bay:'
+
+    @hikes = BayAreaHikes::Hike.ideas
   end
 
   def menu
-    puts 'Please enter the number of the hike you would like to know more about or exit to exit:'
+    input = nil
+    while input != 'exit'
+      puts 'Please enter the number of the hike you would like to know more about or exit to exit:'
+      input = gets.strip.downcase
+
+      if input.to_i > 0
+        puts @hikes[input.to_i - 1]
+      elsif input == 'list'
+        list_hikes
+      elsif input == 'exit'
+        exit
+      else
+        puts 'Not sure what you want.  Please enter a valid number or type list to see entire list.'
+      end
+    end
   end
 
-
+  def goodbye
+    puts 'Thanks for visiting.  Have a great day out there!'
   end
 end

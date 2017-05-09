e3f79a5b2fbdb0a894587fcca96138d26a84e13b
Spencer Evans <spencevans@gmail.com>
2017-05-09 04:23:25 +0000

    added a do loop to the cli

--- lib/bay_area_hikes/cli.rb
+++ lib/bay_area_hikes/cli.rb
@@ -7,9 +7,12 @@ class BayAreaHikes::CLI
   end
 
   def list_hikes
-    puts 'Great ideas for a hike in the Bay:'
+    puts 'Great ideas for a hike in the Bay Area:'
 
     @hikes = BayAreaHikes::Hike.ideas
+    @hikes.each.with_index(1) do |hike, i|
+      puts "#{i}. #{hike.name} - #{hike.county} - #{hike.duration}"
+    end
   end
 
   def menu

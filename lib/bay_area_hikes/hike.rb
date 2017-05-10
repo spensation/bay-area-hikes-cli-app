9f472d0be2bce28242cb4f25ced501e00c1dcec2
Spencer Evans <spencevans@gmail.com>
2017-05-10 01:06:07 +0000

    added second hike

--- lib/bay_area_hikes/hike.rb
+++ lib/bay_area_hikes/hike.rb
@@ -9,6 +9,7 @@ attr_accessor :name, :city, :description
   def self.scrape_hikes
     hikes = []
     hikes << self.scrape_bayarea
+    hikes << self.scrape_bayarea_2
     hikes
   end
 
@@ -21,5 +22,15 @@ attr_accessor :name, :city, :description
     hike.description = doc.search('.entry-content p span').children[3].text[0..359]
     hike
 
+  end
+
+  def self.scrape_bayarea_2
+    doc = Nokogiri::HTML(open('https://www.bayarea.com/play/the-best-bay-area-wildflower-hikes/'))
+#binding.pry
+    hike = self.new
+    hike.name = doc.search('.entry-content h3').children[2].text.strip
+    hike.city = doc.search('.entry-content h3').children[3].text[3..-1]
+    hike.description = doc.search('.entry-content p span').children[7..10].text.strip
+    hike
   end
 end
